7a0938811fb67a10192c0f1459ddd79186edae64
ry6n1k <levchenkoyarya@gmail.com>
2022-08-03 23:36:03 +0300

    fix: port db

--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -7,7 +7,7 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'mysql',
         host: 'localhost',
-        port: 3306,
+        port: 3307,
         username: 'admin',
         password: '123456',
         database: 'test-incode-group',
