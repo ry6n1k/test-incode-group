9f927b7de39bce33a3078e46055d4d4f9100b0f8
ry6n1k <levchenkoyarya@gmail.com>
2022-08-05 12:29:06 +0300

    feat: one-to-many, roleId user_table

---
+++ src/role/role.entity.ts
@@ -0,0 +1,14 @@
+import { User } from './../user/entities/user.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => User, (user) => user.role)
+  user: User[];
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Role } from './../../role/role.entity';
 
 @Entity()
 export class User {
@@ -10,4 +11,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @ManyToOne(() => Role, (role) => role.user)
+  role: Role;
 }
